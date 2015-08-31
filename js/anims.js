9c5015693f034d5d5eb417546411ba816fbbf392
Alastair Dant <alastair@recursiveflow.com>
2015-08-30 22:55:47 -0700

    Commenced work on home page content

--- js/anims.js
+++ js/anims.js
@@ -13,45 +13,20 @@ jQuery(document).ready(function($) {
 
     var r = canvas.width / 4,
         center = compass.point(r*2, r*2),
-        sides = 14,
-        angles = d3.range(sides).map(function (i) {
-          return compass.τ * i/sides
-        })
+        sides = 14;
 
     anim.addStep("draw circle", { onto: circles, at: center, r: r*0.4, dir: "cw", style: "line-b" }, 300);  
 
-    angles.forEach(function (a) {
-      var p1 = compass.point.fromPolar(r*1.2, a, center);
+    d3.range(sides).forEach(function (i) {
+      var a = compass.τ * i/sides,
+          p = compass.point.fromPolar(r*1.2, a, center);
 
-      anim.addStep("draw circle", { onto: circles, at: p1, r: r*0.8, dir: "cw", style: "line-a" }, 30)
+      anim.addStep("draw circle", { onto: circles, at: p, r: r*0.8, dir: "cw", style: "line-a" }, 30)
     })
 
     anim.addStep("draw circle", { onto: circles, at: center, r: r*2.0, dir: "cw", style: "line-b" }, 600);
 
     anim.play();
-  }
-
-  function spanimate (el, width, height) {
-    var container = d3.select(el),
-        discCount = 10,
-        maxSize = Math.min(width, height) / 2;
-
-    d3.range(discCount).forEach(function (i) {
-      var size = (discCount - i) / discCount * maxSize,
-          xPos = Math.round(Math.random() * width),
-          yPos = Math.round(Math.random() * height);
-
-      var disc = container.append("div");
-
-      disc
-        .attr("class", "rflow-logo")
-        .style("width", size + "px") 
-        .style("height", size + "px")
-        .style("left", xPos + "px")
-        .style("top", yPos + "px");
-
-      drawLogo(disc.node());
-    })
   }
 
   function distance (a, b) {
@@ -68,12 +43,9 @@ jQuery(document).ready(function($) {
     var fills = ["#d7d7d7", "#e2e2e2", "#eee"],
         currentFill = 0;
 
-    var tau = 2 * Math.PI,
-        n = Math.min(width, height) >> 1,
+    var n = Math.min(width, height) >> 1,
         particles = new Array(n),
         mouse = { x:0, y:0 };
-
-    console.log(n, "particles")
 
     document.addEventListener("mousemove", function(e){
       mouse.x = Math.min(width, Math.max(0, e.x));
@@ -91,7 +63,7 @@ jQuery(document).ready(function($) {
       };
     }
 
-    function render(elapsed) {
+    function render (elapsed) {
       context.save();
       context.clearRect(0, 0, width, height);
 
@@ -109,7 +81,7 @@ jQuery(document).ready(function($) {
 
         context.beginPath();
         context.fillStyle = p.fill;
-        context.arc(p.x, p.y, p.r * proximityFactor, 0, tau);
+        context.arc(p.x, p.y, p.r * proximityFactor, 0, compass.τ);
         context.fill();
       }
 
@@ -128,8 +100,7 @@ jQuery(document).ready(function($) {
     $anim.attr("width", containerWidth + "px");
     $anim.attr("height", containerHeight + "px");
     
-    // animate($anim[0], containerWidth, containerHeight);
-    animate(document.querySelector(".rflow-page-anim"));
+    animate($anim[0]);
   }, 1000);
 
 })
